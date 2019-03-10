78868add7a823feda8eb21d5b4eb4b6583ca83f7
luis-otavio <luisotavio05@gmail.com>
2019-03-10 17:59:43 -0300

    button search

--- src/components/Search/index.js
+++ src/components/Search/index.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import { Platform } from "react-native";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 
 export default class Search extends Component {
   render() {
-    return <GooglePlacesAutocomplete 
-        placeHolder='Para onde vamos?'
-        placeHolderTextColor="#333"
+    return <GooglePlacesAutocomplete
+        placeholder="Para onde?"
+        placeholderTextColor="#333"
         onPress={() => {}}
         query={{
-            key: 'AIzaSyCkC1D5PfvI2OOVS6oVGn9a6gNjdTsKwDU',
+            key: 'AIzaSyCkC1D5PfvI2OOVS6oVGn9a6gNjdTsKwDU', //sua chave do google platform
             language: 'pt'
         }}
         textInputProps={{
@@ -17,6 +18,60 @@ export default class Search extends Component {
         }}
         fetchDetails
         enablePoweredByContainer={false}
+        styles={{
+          container: {
+            position: 'absolute',
+            top: Platform.select({ ios: 40, android: 50 }),
+            width: '100%'
+          },
+          textInputContainer: {
+            flex: 1,
+            backgroundColor: "transparent",
+            height: 54,
+            marginHorizontal: 20,
+            borderTopWidth: 0,
+            borderBottomWidth: 0
+          },
+          textInput: {
+            height: 54,
+            margin: 0,
+            borderRadius: 0,
+            paddingTop: 0,
+            paddingBottom: 0,
+            paddingLeft: 20,
+            paddingRight: 20,
+            marginTop: 0,
+            marginLeft: 0,
+            marginRight: 0,
+            elevation: 5,
+            shadowColor: "#000",
+            shadowOpacity: 0.1,
+            shadowOffset: { x: 0, y: 0 },
+            shadowRadius: 15,
+            borderWidth: 1,
+            borderColor: "#DDD",
+            fontSize: 18
+          },
+          listView: {
+            borderWidth: 1,
+            borderColor: "#DDD",
+            backgroundColor: "#FFF",
+            marginHorizontal: 20,
+            elevation: 5,
+            shadowColor: "#000",
+            shadowOpacity: 0.1,
+            shadowOffset: { x: 0, y: 0 },
+            shadowRadius: 15,
+            marginTop: 10
+          },
+          description: {
+            fontSize: 16
+          },
+          row: {
+            padding: 20,
+            height: 58
+          }
+        }}
     />;
   }
 }
